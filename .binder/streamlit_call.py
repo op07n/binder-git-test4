0667f097f25acb50aeb905b003056b2225b8fe6a
op07n <39182105+op07n@users.noreply.github.com>
2020-01-09 10:37:50 +0100

    Update streamlit_call.py

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -3,5 +3,5 @@ import os
 
 def load_jupyter_server_extension(nbapp):
     """serve the streamlit app"""
-    os.chdir('PuzzleSolver/')
+    os.chdir('demoapps/PuzzleSolver/')
     Popen(["streamlit", "run", "app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
